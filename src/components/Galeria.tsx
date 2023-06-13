71159848484e7991796f83fd0a6e568d341ab931
Elías de Francisco <eliasdefrancisco@gmail.com>
2023-06-13 20:47:29 +0200

    download images

--- src/components/Galeria.tsx
+++ src/components/Galeria.tsx
@@ -2,6 +2,7 @@ import { signal } from '@preact/signals'
 import { BucketDataJson, imageTypes } from 'src/types'
 import styles from './Galeria.module.css'
 import FullScreenImage from './FullScreenImage'
+import { imageTailNames } from '../types'
 
 type Props = {
 	bucketData: BucketDataJson
@@ -17,13 +18,47 @@ export default function Galeria ({ bucketData, baseUrl, tipo }: Props) {
 		? `${baseUrl}/${bucketData.urlBaseRelativa}`
 		: `${baseUrl}/${bucketData.thumbnail.urlBaseRelativa}`
 
+	const originalUrlBase = tipo === imageTypes.fotocall_gifs
+		? `${baseUrl}/${bucketData.urlBaseRelativa}`
+		: `${baseUrl}/${bucketData.original.urlBaseRelativa}`
+
 	const thumbnailFiles = tipo === imageTypes.fotocall_gifs
 		? bucketData.archivos
 		: bucketData.thumbnail.archivos
 
-	function handleImageClick (imageName: string) {
-		imageFullScreen.value = imageName
+	function handleImageClick (imageThumbName: string) {
+		imageFullScreen.value = imageThumbName
 	}
+
+	function handleDownload (imageThumbName: string) {
+		const imageOriginalName = imageThumbName
+			.replace(imageTailNames.thumbnail, '')
+			.replace('.webp', '.jpg')
+		const imageOriginalUrl = `${originalUrlBase}${imageOriginalName}`
+
+		fetch(imageOriginalUrl)
+			.then(respuesta => respuesta.blob())
+			.then(blob => {
+				// Crear un objeto URL para el blob
+				const urlBlob = window.URL.createObjectURL(blob)
+
+				// Crear un enlace para la descarga
+				const enlace = document.createElement('a')
+				enlace.href = urlBlob
+				enlace.download = 'boda_marina_y_elias.jpg'
+
+				// Añadir el enlace al cuerpo del documento (necesario para Firefox)
+				document.body.appendChild(enlace)
+
+				// Simular un clic en el enlace
+				enlace.click()
+
+				// Eliminar el enlace del cuerpo del documento después de descargar la imagen
+				document.body.removeChild(enlace)
+			})
+			.catch(error => console.error('Error:', error))
+	}
+
 
 	return (
 		<div class={styles.container}>
@@ -41,15 +76,11 @@ export default function Galeria ({ bucketData, baseUrl, tipo }: Props) {
 							<span
 								class={styles.buttonShare}
 								onClick={() => (lastClickOn.value = 'Share')}
-							>
-									🥎
-							</span>
+							>🥎</span>
 							<span
 								class={styles.buttonDownload}
-								onClick={() => (lastClickOn.value = 'Download')}
-							>
-								⬇️
-							</span>
+								onClick={() => handleDownload(imageName)}
+							>⬇️</span>
 						</div>
 					</li>
 				))}
